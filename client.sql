1bc2448a9f64f689d7da0faec27b0fb3598d4ffd
nouhamenadjlia <nouha.menadjlia@hotmail.fr>
2022-06-02 10:53:24 +0200

    Creation de la BD

---
+++ client.sql
@@ -0,0 +1,12 @@
+--Authentification/ connexion
+mysql -u root --utilisateur ne posséde pas de mot de passe
+--mysql -u root -p --utilisateur  posséde pas de mot de passe
+-- lister les bases d edonnées existante
+SHOW DATABASES;
+-- Creation de la BD
+CREATE DATABASE Ventes;
+
+
+
+
+--Creation de la table clients
